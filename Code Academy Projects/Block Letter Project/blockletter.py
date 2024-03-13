8ef727cfb64847e36478f2366121ec065c44334c
Colton <coltonjorgenson@Coltons-MBP.attlocal.net>
2024-03-13 00:52:25 -0500

    adding new code academy projects

---
+++ Code Academy Projects/Block Letter Project/blockletter.py
@@ -0,0 +1,19 @@
+letter_c1 =' CCC ''  JJJJJ' 
+letter_c2 ='C   C''    J  '
+letter_c3 ='C''        J  ' 
+letter_c4 ='C''        J  '
+letter_c5 ='C''      J J  '
+letter_c6 ='C   C''  J J  '
+letter_c7 =' CCC ''   JJ  '
+print(letter_c1 + '\n' + letter_c2 + '\n' + letter_c3 + '\n' + letter_c4 + '\n' + letter_c5 + '\n' + letter_c6 + '\n' + letter_c7)
+#colton jorgenson
+#I'm excited to learn Python
+
+#alternate example
+print("  SSS   L     ")
+print(" S   S  L     ")
+print(" S      L     ")
+print("  SSS   L     ")
+print("     S  L     ")
+print(" S   S  L     ")
+print("  SSS   LLLLL ")
